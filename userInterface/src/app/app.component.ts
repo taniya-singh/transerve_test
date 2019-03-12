300a70b9d55695d33c0cb9d85a9a3182b75a0d9c
taniya <taniya.sk92@gmail.com>
2019-03-13 00:00:20 +0530

    first commit

---
+++ userInterface/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'My First Angular App!';
+}
---
+++ userInterface/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {routes} from './app.routing'
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
